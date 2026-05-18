cab68ddc77714a38725c646ba1b35e8b98768567
Abdurrahman-Kavak <apokvk03@gmail.com>
2026-05-18 14:39:35 +0300

    posts router CRUD işlemleri, CORS desteği ve gitignore ayarları eklendi

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,101 @@
 // posts için gerekli routerları buraya yazın
+const express = require("express");
+const Posts = require("./posts-model");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ message: "Gönderiler alınamadı" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Gönderi bilgisi alınamadı" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return res
+      .status(400)
+      .json({ message: "Lütfen gönderi için title ve contents sağlayın" });
+  }
+  try {
+    const { id } = await Posts.insert({ title, contents });
+    const newPost = await Posts.findById(id);
+    res.status(201).json(newPost);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Veritabanına kaydedilirken bir hata oluştu" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return res
+      .status(400)
+      .json({ message: "Lütfen gönderi için title ve contents sağlayın" });
+  }
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    } else {
+      await Posts.update(req.params.id, { title, contents });
+      const updatedPost = await Posts.findById(req.params.id);
+      res.status(200).json(updatedPost);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Gönderi bilgileri güncellenemedi" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "Belirtilen ID li gönderi bulunamadı" });
+    }
+
+    await Posts.remove(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ message: "Gönderi silinemedi" });
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "Girilen ID'li gönderi bulunamadı." });
+    }
+
+    const comments = await Posts.findPostComments(req.params.id);
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json({ message: "Yorumlar bilgisi getirilemedi" });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
+
+module.exports = server;
